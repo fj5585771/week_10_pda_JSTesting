cc3050685a5b86243f1ff134df590ccf29cb23b8
fraser jamieson <fraserjamieson.93@gmail.com>
2021-01-22 16:49:17 +0000

    unit testing completed

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -17,4 +17,44 @@ describe('calculator', function () {
     assert.strictEqual(calculator.runningTotal, 5);
   })
 
+  it('can subtract 4 from 5 to get 1', function(){
+    calculator.previousTotal = 5;
+    calculator.subtract(4);
+    assert.strictEqual(calculator.runningTotal, 1);
+  })
+
+  it('can multiply 2 by 2 to get 4', function(){
+    calculator.previousTotal = 2;
+    calculator.multiply(2);
+    assert.strictEqual(calculator.runningTotal, 4);
+  })
+
+  it('can divide 5 by 15 and get 3', function(){
+    calculator.previousTotal = 15;
+    calculator.divide(5);
+    assert.strictEqual(calculator.runningTotal, 3);
+  })
+
+  it('can show numberClicking format correctly', function(){
+    calculator.runningTotal = 0;
+    calculator.numberClick(3);
+    assert.strictEqual(calculator.newTotal, false);
+  })
+
+  it('can show operatorClick working correctly', function(){
+    calculator.previousTotal = 8;
+    calculator.previousOperator = '/';
+    calculator.divide(4);
+    calculator.operatorClick('=');
+    calculator.newTotal = true;
+    calculator.runningTotal = 2;
+    assert.strictEqual(calculator.newTotal, true);
+  })
+
+  it('can clear the operation in effect', function(){
+    calculator.runningTotal = 0;
+    calculator.clearClick();
+    assert.strictEqual(calculator.runningTotal, 0);
+  })
+
 });
