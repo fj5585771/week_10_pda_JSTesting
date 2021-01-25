2614c3de8c4986d700c7e9e06d3bd02a8725af40
fraser jamieson <fraserjamieson.93@gmail.com>
2021-01-25 17:27:58 +0000

    2nd test passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -18,9 +18,30 @@ describe('calculator functionality', function() {
 
   it('should allow operators to give result of previous operation', () => {
     running_total = element(by.css('#running_total'))
-    element(by.css('#number2')).click();
-    operators = element(by.css('.operator'))
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    element(by.css('#number8')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('64');
   })
+
+  // Can multiple operations be chained together?
+
+  it('should allow multiple operations to be chained together', () => {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('32');
+  })
+
+
+  // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
+  // What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+  // 
 
 });
